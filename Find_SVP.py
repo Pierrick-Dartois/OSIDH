d1f512ee139e50338b7be76cb6dec84b85c562b3
Pierrick Dartois <pierrickdartois@icloud.com>
2021-06-29 17:33:32 +0200

    Execution of BKZ with k=42

--- Find_SVP.py
+++ Find_SVP.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append('Documents/Codes/OSIDH')
 from sage.all import *
 from fpylll import *
+from time import time
 
 with open("Documents/Codes/OSIDH/Relations_lattice_basis.txt","r",encoding="utf-8") as f:
 	L=f.readlines()
@@ -17,13 +18,15 @@ with open("Documents/Codes/OSIDH/Relations_lattice_basis.txt","r",encoding="utf-
 		row=x.split(",")
 		M.append([int(y) for y in row])
 
-M=IntegerMatrix.from_matrix(M).transpose()
-M=LLL.reduction(M)
-u=SVP.shortest_vector(M)
-print(u)
-#M_bkz = BKZ.reduction(M.transpose(), BKZ.Param(3))
-#L_max=[]
-#for i in range(74):
-	#Li=list(M_bkz[i])
-	#L_max.append(max(max(Li),-min(Li)))
-#print(min(L_max))
+M=IntegerMatrix.from_matrix(M)
+
+t1=time()
+M_bkz = BKZ.reduction(M.transpose(), BKZ.Param(42))
+t2=time()
+print("Time BKZ: {0} s".format(t2-t1))
+
+# Saving the BKZ matrix (outputted lattice vectors are rows, unlike entry lattice vectors of M given in columns) 
+with open("Documents/Codes/OSIDH/Lattice_BKZ_42.txt","w",encoding="utf-8") as f:
+	f.write(str(t)+"\n")
+	for i in range(t):
+		f.write(str(M_bkz[i])[1:-1]+"\n")
